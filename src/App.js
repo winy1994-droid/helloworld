273ae48e61588c5f744bad5109fcb6789e81556f
hasha <137384828+winy1994-droid@users.noreply.github.com>
2024-01-10 16:17:18 +0330

    functional component in react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Component } from "react";
+// import { Component } from "react";
+ 
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <h1>hassan</h1>
+//         <h2>hasha</h2>
+//         <h3>hello world</h3>
+//       </>
+//     );
+//   }
+// }
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>hassan</h1>
-        <h2>hasha</h2>
-        <h3>hello world</h3>
-      </>
-    );
-  }
+function App(){
+    return(
+        <>
+        <h1>hasha</h1>
+        <h1>hello</h1>
+        </>
+    )
 }
 
 export default App;
