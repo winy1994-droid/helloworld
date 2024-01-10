a3af6a088efe757e63fdc6e3f0685de66ea87f8a
hasha <137384828+winy1994-droid@users.noreply.github.com>
2024-01-10 16:09:13 +0330

    classBase component in react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Component } from "react";
 
-class App extends Component{
+class App extends Component {
   render() {
-    return <h1>hello world</h1>;
+    return (
+      <>
+        <h1>hassan</h1>
+        <h2>hasha</h2>
+        <h3>hello world</h3>
+      </>
+    );
   }
 }
 
